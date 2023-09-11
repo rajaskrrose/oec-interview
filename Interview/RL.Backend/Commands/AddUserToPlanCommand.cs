ddea3442d7e1c136c3baab41f23a37676ca1f77f
rajaskrrose <144654535+rajaskrrose@users.noreply.github.com>
2023-09-11 14:49:26 +0530

    Add files via upload

---
+++ Interview/RL.Backend/Commands/AddUserToPlanCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using RL.Backend.Models;
+
+namespace RL.Backend.Commands
+{
+    public class AddUserToPlanCommand : IRequest<ApiResponse<Unit>>
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public int PlanId { get; set; }
+        public int ProcedureId { get; set; }
+    }
+}
