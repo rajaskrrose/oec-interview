2430c124f1997a5af4c2873f1da8afaaabcb3b6a
rajaskrrose <144654535+rajaskrrose@users.noreply.github.com>
2023-09-11 14:39:37 +0530

    Update PlanProcedureItem.jsx

--- interview-frontend/src/components/Plan/PlanProcedureItem/PlanProcedureItem.jsx
+++ interview-frontend/src/components/Plan/PlanProcedureItem/PlanProcedureItem.jsx
@@ -1,14 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactSelect from "react-select";
+import {
+    addUserToPlan, getPlanProcedureUsers
+} from "../../../api/api";
 
-const PlanProcedureItem = ({ procedure, users }) => {
+const PlanProcedureItem = ({ planProcedure, procedure, users }) => {
     const [selectedUsers, setSelectedUsers] = useState(null);
+    useEffect(() => {
+        (async () => {
+            var userOptions = [];
+            setSelectedUsers(null);
+            console.log(procedure.procedureId + planProcedure.planId);
+            var UsersfromPLan = await getPlanProcedureUsers(planProcedure.planId, planProcedure.procedureId);
+            console.log(UsersfromPLan);
+            for (var i = 0; i < UsersfromPLan.length; i++) {
+                userOptions.push({ label: UsersfromPLan[i].name, value: UsersfromPLan[i].userId });
 
-    const handleAssignUserToProcedure = (e) => {
+            }
+            console.log(userOptions);
+            setSelectedUsers(userOptions);
+            console.log(selectedUsers);
+            console.log(users);
+
+        })();
+    }, [planProcedure]);
+
+    const handleAssignUserToProcedure = async (e) => {
         setSelectedUsers(e);
+        //setselectedProcedureid(procedure);
         // TODO: Remove console.log and add missing logic
-        console.log(e);
+
+        var userOptions = [];
+        console.log(typeof (userOptions));
+        if (e.length > 0) {
+            console.log(e);
+            e.map((e) => userOptions.push({ label: e.label, value: e.value }));
+            console.log(userOptions);
+            await addUserToPlan(userOptions, planProcedure);
+        }
+
     };
+
+
 
     return (
         <div className="py-2">
